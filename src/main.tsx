956d9cd199d4812a3f36a537b04355f586390c06
champln <jamadhar99@hotmail.com>
2026-09-10 23:01:47 +0700

    Exclude CMMS from public website build

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,10 @@
-import { StrictMode, useEffect, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import CMMSApp from "./CMMSApp";
 import "./styles.css";
-import "./cmms.css";
-import "./iot-monitor.css";
-
-function RootRouter() {
-  const [isCmms, setIsCmms] = useState(() =>
-    window.location.hash.startsWith("#/cmms"),
-  );
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      setIsCmms(window.location.hash.startsWith("#/cmms"));
-    };
-
-    window.addEventListener("hashchange", handleHashChange);
-    return () => window.removeEventListener("hashchange", handleHashChange);
-  }, []);
-
-  return isCmms ? <CMMSApp /> : <App />;
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RootRouter />
+    <App />
   </StrictMode>,
 );
